79bd7b174c899c757acafeae7abcd97a50058c0c
ahyangnb <zonggeu@gmail.com>
2019-12-30 23:31:25 +0800

    commit local

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_flutter/page/tiktok_page.dart';
-void main() => runApp(MyApp());
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Tiktok Gestures",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
         canvasColor: Colors.black,
       ),
